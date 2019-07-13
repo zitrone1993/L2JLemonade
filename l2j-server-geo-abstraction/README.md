3ceb1e915cd0246ea565b320ea7be539007a8cb4
Zitrone <lupone730@gmail.com>
2019-07-13 17:24:15 +0300

    Add files via upload

---
+++ l2j-server-geo-abstraction/README.md
@@ -0,0 +1,20 @@
+L2J Server: Geo Abstraction
+===
+
+L2J Server: Geo Abstraction is an abstraction layer for Geo Driver implementation.
+
+
+Links
+---
+
+- [Web Site](http://www.l2jserver.com)
+
+- [Forums](http://www.l2jserver.com/forum/)
+
+- [Live Support](https://gitter.im/L2J/L2J_Server)
+
+- [Downloads](http://www.l2jserver.com/#downloads)
+
+- [Documentation](https://bitbucket.org/l2jserver/l2j_server/wiki)
+
+- [@l2jserver](https://twitter.com/l2jserver)
---
+++ l2j-server-geo-abstraction/src/main/java/com/l2jserver/gameserver/geoengine/NullDriver.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2019 L2J Server
+ * 
+ * This file is part of L2J Server.
+ * 
+ * L2J Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.l2jserver.gameserver.geoengine;
+
+import java.util.Properties;
+
+import com.l2jserver.gameserver.geoengine.abstraction.IGeoDriver;
+
+/**
+ * Null driver.
+ * @author HorridoJoho
+ */
+public final class NullDriver implements IGeoDriver {
+	
+	public NullDriver(Properties props) {
+		// Do nothing.
+	}
+	
+	@Override
+	public int getGeoX(int worldX) {
+		return worldX;
+	}
+	
+	@Override
+	public int getGeoY(int worldY) {
+		return worldY;
+	}
+	
+	@Override
+	public int getWorldX(int geoX) {
+		return geoX;
+	}
+	
+	@Override
+	public int getWorldY(int geoY) {
+		return geoY;
+	}
+	
+	@Override
+	public boolean hasGeoPos(int geoX, int geoY) {
+		return false;
+	}
+	
+	@Override
+	public int getNearestZ(int geoX, int geoY, int worldZ) {
+		return worldZ;
+	}
+	
+	@Override
+	public int getNextLowerZ(int geoX, int geoY, int worldZ) {
+		return worldZ;
+	}
+	
+	@Override
+	public int getNextHigherZ(int geoX, int geoY, int worldZ) {
+		return worldZ;
+	}
+	
+	@Override
+	public boolean canEnterNeighbors(int geoX, int geoY, int worldZ, Direction first, Direction... more) {
+		return true;
+	}
+	
+	@Override
+	public boolean canEnterAllNeighbors(int geoX, int geoY, int worldZ) {
+		return true;
+	}
+}
